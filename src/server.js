a4ec3e7c8f9bc6060e6134712273fe16c29dd2e3
WannyPark <adgh4728@naver.com>
2023-07-28 17:44:44 +0900

    23-07-28

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import morgan from "morgan";
+
+const PORT = 4000;
+const app = express();
+const logger = morgan("dev");
+
+const handleHome = (req, res) => {
+  return res.send("Hello!");
+}
+const login = (req, res) => {
+  return res.send("login");
+}
+app.use(logger);
+app.get("/", handleHome);
+app.get("/login", login);
+
+const handleListening = () => {
+  console.log(`Server listening on port localhost:${PORT}`);
+}
+app.listen(PORT, handleListening);
